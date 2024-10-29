9c773443cf3be8ea90419173567a5d601bc6ce56
Ribeiro-Gustavo <engs-gustavoribeiro@camporeal.edu.br>
2024-10-28 21:33:57 -0300

    att

--- app.js
+++ app.js
@@ -1,75 +1,83 @@
-// importando express
-const express = require("express");
+//importando express
+const express = require('express');
 
 //cookies e sessions
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const { redirect } = require('react-router-dom');
 
-//inicializar express
+//inicializa express
 const app = express();
 
 app.use(cookieParser());
-
 app.use(session(
     {
-        secret: "123456789", // chave para acessar os cookies
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false } // habilitar cookies over HTTPS
+        secret: 'minhachave', //chave para acessar os cookies
+        resave: false, //evita gravar sessões sem alterações
+        saveUninitialized: true //salvar na guia anonima
     }
 ));
 
-// dados de exemplo
+//dados de exemplo
 const produtos = [
-        {id: 1, nome: "Notebook", preco: 2500,},
-        { id: 2 , nome: "Celular", preco:3500,},
-        { id: 3, nome: "Tablet", preco: 1500,},
-        { id: 4 , nome: "Videogame", preco:5000,}
-    ];
+    {id: 1, nome: 'Alface' , preco: 2},
+    {id: 2,nome: 'Pashimina' , preco: 70},
+    {id: 3, nome: 'Pastel' , preco: 6}
+];
 
-app.get('/produtos', (req, res) => {
+app.get('/produtos', (req,res) => {
     res.send(`
-        <h1>Produtos</h1>
+        
+        <h1>Lista de produtos</h1>
         <ul>
-        <li>
-            ${produtos.map(produto => `<li>${produto.nome} - R$ ${produto.preco}</li>`).join("")}
-            <a href="/adicionar/${produtos.id}
-            ">Adicionar ao carrinho</a>
-        </li>
-        </ul>
-        <a href="/carrinho">Carrinho</a>
-    `)
-});
 
-//rota de adicionar produto 
-        app.get("/adicionar/:id", (req, res) => {
-            const id = parseInt(req.params.id);
-
-            const produto = produtos.find((p) => p.id === id);
-
-            if(produto) {
-                if (!req.session.carrinho){
-                    req.session.carrinho = [];
-                }
-                req.session.carrinho.push(produto);
+            ${
+                produtos.map((produto) => `
+                    <li>${produto.nome} - ${produto.preco} <a href="/adicionar/${produto.id}">Adicionar ao Carrinho</a></li>
+                `).join('')
             }
 
-            res.redirect("/produtos");
-        });
+        </ul>
+        <a href="/carrinho">Ver Carrinho</a>
 
-// rota do carrinho
-app.get("/carrinho", (req, res) => {
+    `);
+});
+
+//rota de adicionar produto
+app.get('/adicionar/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+
+    const produto = produtos.find((p) => p.id === id );
+
+    if(produto) {
+        if(!req.session.carrinho){
+            req.session.carrinho = [];
+        }
+        req.session.carrinho.push(produto);
+    }
+
+    res.redirect('/produtos');
+});
+
+//rota do carrinho
+app.get('/carrinho', (req, res) => {
     const carrinho = req.session.carrinho || [];
 
     res.send(`
+        
         <h1>Carrinho</h1>
         <ul>
-        <li>
-            ${carrinho.map(produtos => `<li>${produtos.nome} - R$ ${produtos.preco}</li>`).join("")}
-        </li>
+
+            ${
+                carrinho.map((produto) => `
+                    <li>${produto.nome} - ${produto.preco} <a href="/adicionar/${produto.id}">Adicionar ao Carrinho</a></li>
+                `).join('')
+            }
+
         </ul>
-        <a href="/finalizar">Finalizar compra</a>
-    `)
-})
+        <a href="/produtos">Continuar Comprando</a>
+
+    `);
+});
 
 app.listen(8080);
